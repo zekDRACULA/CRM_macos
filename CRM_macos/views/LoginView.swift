295939a233eba47c529b1ae883b82689403da904
ZekDRACULA <84472796+zekDRACULA@users.noreply.github.com>
2024-08-18 18:03:30 +0530

    Now not showing navigation Bar back button
    not showing of sidebar still persists

--- CRM_macos/views/LoginView.swift
+++ CRM_macos/views/LoginView.swift
@@ -54,39 +54,37 @@ class UserCredentialForm : ObservableObject{
 struct LoginFormView : View {
     @ObservedObject var view = UserCredentialForm.shared
     var body: some View {
-        NavigationStack {
-            VStack{
-                Text("Login")
-                    .font(.system(size: 17, weight: .bold))
-                Form{
-                    TextField("Email", text: $view.email)
-                        .frame(width: (NSScreen.main?.frame.width ?? 0)/3 ,height: 40)
-                    TextField("Password", text: $view.password)
-                        .frame(width: (NSScreen.main?.frame.width ?? 0)/3 ,height: 40)
-                    HStack{
-                        Button(action: {
-                            view.matchCredentiails()
-                        }, label: {
-                            Text("Login")
-                        })
-                        NavigationLink {
-                            RegisterView()
-                        } label: {
-                            Text("Register")
-                        }
+        if(view.isAuthenticated){
+            Dashboard()
+        }else {
+            NavigationStack {
+                VStack{
+                    Text("Login")
+                        .font(.system(size: 17, weight: .bold))
+                    Form{
+                        TextField("Email", text: $view.email)
+                            .frame(width: (NSScreen.main?.frame.width ?? 0)/3 ,height: 40)
+                        TextField("Password", text: $view.password)
+                            .frame(width: (NSScreen.main?.frame.width ?? 0)/3 ,height: 40)
+                        HStack{
+                            Button(action: {
+                                view.matchCredentiails()
+                            }, label: {
+                                Text("Login")
+                            })
+                            NavigationLink {
+                                RegisterView()
+                            } label: {
+                                Text("Register")
+                            }
 
+                        }
                     }
+                    .frame(width: (NSScreen.main?.frame.width ?? 0) / 2,
+                           height: (NSScreen.main?.frame.height ?? 0) / 2)
+                    .background(BlurView())
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
                 }
-                .frame(width: (NSScreen.main?.frame.width ?? 0) / 2,
-                       height: (NSScreen.main?.frame.height ?? 0) / 2)
-                .background(BlurView())
-                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
-                
-               // NavigationLink("", destination : Dashboard(), isActive: $view.isAuthenticated)
-                .navigationDestination(isPresented: $view.isAuthenticated) {
-                    Dashboard()
-                }
-                
             }
         }
         
